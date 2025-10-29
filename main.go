082ef3debe5b4f3cf8450d571ed703093f12e0a2
Luni <65511597+lunicarus@users.noreply.github.com>
2025-10-29 13:10:44 -0300

    Refactor : Fix formatting in PrintBoilingPointKelvinToCelcius
    Minor Detail for better printing

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func PrintBoilingPointKelvinToCelcius() {
 	CelciusBoilingPoint := 100
 	KelvinBoilingPoint := float32(CelciusBoilingPoint) + 273.15
-	fmt.Printf("Water Boiling Point n Kelvin: %g Water Boiling Point in Celcius is %d", KelvinBoilingPoint, CelciusBoilingPoint)
+	fmt.Printf("Water Boiling Point n Kelvin: %g\nWater Boiling Point in Celcius is %d", KelvinBoilingPoint, CelciusBoilingPoint)
 }
 
 func main() {
